37d1d32f1893c51d794d7c2597fb50052c932ac3
CarlosT25-png <cartorresva@gmail.com>
2024-06-18 23:32:51 -0500

    login and signup added

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import ConversationDashboard from "@/components/Chat/ConversationDashboard";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <ConversationDashboard />
-  );
-}
--- src/components/Typography/H1.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function H1({ children }: React.PropsWithChildren ) {
-  return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-      {children}
-    </h1>
-  )
-}
